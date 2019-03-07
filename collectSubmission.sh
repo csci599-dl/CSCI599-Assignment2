aee5470a37a803c11b0d3ae9e767577603e6ea75
telin0411 <eggnome0411@gmail.com>
2019-03-07 15:34:21 -0800

    update sh

--- collectSubmission.sh
+++ collectSubmission.sh
@@ -1,2 +1,2 @@
 rm -f assignment2.zip 
-zip -r assignment2.zip . -i "*.ipynb" "lib/*.py" "lib/tf_models/*.py" -x "*.ipynb_checkpoints*"
+zip -r assignment2.zip . -i "Problem_*.ipynb" "lib/*.py" "lib/tf_models/*.py" -x "*.ipynb_checkpoints*"
